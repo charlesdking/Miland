8c5ed7150d43983349be5073f0eed087abf63d15
Charles King <charlesdaveking@gmail.com>
2026-01-03 20:15:26 -0600

    Initial Commit

---
+++ Miland/MilandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilandApp.swift
+//  Miland
+//
+//  Created by Charles King on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MilandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
